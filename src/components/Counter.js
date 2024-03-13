f9e50afb2adb6626788a55b14c9ce9b121f8ed43
Anujeet Swain <anujeet98@gmail.com>
2024-03-14 05:02:04 +0530

    use redux state in react component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,32 @@
 import classes from './Counter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const dispatch = useDispatch();
+  const counter = useSelector(state=>state.counter);
+
+
+  const toggleCounterHandler = () => {
+
+  };
+
+  const incrementCounterHandler = () => {
+    dispatch({type: 'INCREMENTBY2'});
+  };
+
+  const decrementCounterHandler = () => {
+    dispatch({type: 'DECREMENTBY2'});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+      <button className={classes.CounterBtn} onClick={incrementCounterHandler}>Increment</button>
+      <button className={classes.CounterBtn} onClick={decrementCounterHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
